3a23b3ccda5a4e910171c02b1d05277633fd00aa
efvalera <efvalera2014@gmail.com>
2024-07-20 19:04:07 +1000

    Add files via upload

---
+++ Task 6.py
@@ -0,0 +1,5 @@
+done_HW = 12
+wasted_hours = 1.5
+course_name = "Python"
+one_task_time = wasted_hours / done_HW
+print ("Курс: ", course_name, ",", "всего задач:", done_HW, ",", "затрачено часов:", wasted_hours, ",", "среднее время выполнения ", one_task_time, "часа.")
